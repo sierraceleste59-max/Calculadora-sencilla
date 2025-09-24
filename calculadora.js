ab1169f3ca75c1750265230de7e2ca8234bf986f
sierraceleste59-max <sierraceleste59@gmail.com>
2025-09-24 17:22:06 -0300

    U

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+document.getElementById('display').innerText = '0';
+
+const btnNum1 = document.querySelector('.#num1');
+console.log(btnNum1);
+
+btnNum1.addEventListener('click', function() {
+    document.getElementById('display').innerText = '1';
+    num1=0;
+    
+
+}
